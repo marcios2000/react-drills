a6d76226e9c6d9394b6030be9e95c5fb71c4dce4
Marcio Silva <marciotexas@yahoo.com>
2019-05-14 15:21:21 -0500

    initial

---
+++ app-5/src/Image.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+
+class Image extends Component {
+    render() {
+        return (
+            <div>
+                <img src={"/Users/marciosilva/devmtn/react-drills/app-5/src/earth.jpg"} alt="foto"/>
+            <caption> Error 599</caption>
+            </div>
+            
+        )
+    }
+}
+
+
+
+
+
+
+
+
+export default Image;
